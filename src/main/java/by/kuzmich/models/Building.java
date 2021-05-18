aeb326bc30c5fb817175fd30163508fcc00f0974
Vadzim Kuzmich'$ git config --global user.email vk.chgk@gmail.comgit config --global user.name Vadzim <vk.chgk@gmail.com>
2021-05-18 13:53:11 +0300

    Fixed bug

--- src/main/java/by/kuzmich/models/Building.java
+++ src/main/java/by/kuzmich/models/Building.java
@@ -61,11 +61,6 @@ public class Building {
         elevators.forEach(elevator -> {
             elevator.setCallService(callService);
             elevator.setFloorService(serviceFloor);
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
             elevator.start();
         });
     }
